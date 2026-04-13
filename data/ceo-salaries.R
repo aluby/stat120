d019d016417b5631851d5c7e05d1cb4867b45a44
Amanda Luby <aluby@carleton.edu>
2026-04-13 09:22:05 -0500

    updates for mon

--- data/ceo-salaries.R
+++ data/ceo-salaries.R
@@ -8,3 +8,18 @@ ceo_salaries %>%
   select(Year, Ticker, Company, CEO, Pay) %>%
   write_csv("ceo_salaries_23_24.csv")
 
+# Source: https://dasl.datadescription.com/datafile/ceo-compensation-2024/
+ceo_salaries_2024 <- read_delim("~/Downloads/ceo-compensation-2024.txt") |>
+  mutate(
+    ceo_comp = `Compensation ($M)`*1000000
+  ) |>
+  select(
+    ticker = Ticker,
+    company = Company,
+    worker_pay = `Median worker pay`,
+    ceo_comp
+  )
+
+ggplot(ceo_salaries_2024, aes(x = worker_pay, y = ceo_comp/1000000)) + 
+  geom_point() +
+  geom_smooth()
